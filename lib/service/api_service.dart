38a576453042e84503f0f0fdea03c9658205c27a
lebaron567 <emericbarotin@gmail.com>
2025-02-17 15:07:47 +0100

    Add ApiService class for handling sign-in and sign-up API calls

---
+++ lib/service/api_service.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl;
+
+  ApiService({required this.baseUrl});
+
+  Future<http.Response> signIn(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/signin'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': email,
+        'password': password,
+      }),
+    );
+    return response;
+  }
+
+  Future<http.Response> signUp(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/signup'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': email,
+        'password': password,
+      }),
+    );
+    return response;
+  }
+
+  // Ajoutez d'autres méthodes pour interagir avec votre API
+}
