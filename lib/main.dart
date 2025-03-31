07ce72298b4f23552c8a43bb07aadc23437d27d5
Evan Skp <evanskp@MacBook-Air-de-Evan.local>
2025-03-31 15:56:01 +0200

    profile debut

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../views/auth/login_page.dart';
 //import './views/feed/feed_page.dart';
 import './providers/auth_provider.dart';
+import './views/profile/profile_page.dart';
+import './views/auth/register_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -19,8 +21,18 @@ class MyApp extends ConsumerWidget {
       title: 'Instagram Bis',
       debugShowCheckedModeBanner: false,
       routes: {
-        '/login': (context) => const LoginPage(),
+        '/': (context) => UserProfile(user: {
+          'username': 'John Doe',
+          'avatar': 'https://example.com/avatar.jpg',
+          'followers': 100,
+          'following': 50,
+          'bio': 'This is a sample bio.',
+        }),
+        // Remplacez 'userId' par l'ID utilisateur réel
+        // Remplacez '12345' par l'ID utilisateur réel
+        //'/login': (context) => const LoginPage(),
         //'/feed': (context) => const FeedPage(),
+        //'/': (context) => const RegisterPage(),
         // autres routes...
       },
       //home: isAuthenticated ? const FeedPage() : const LoginPage(),
